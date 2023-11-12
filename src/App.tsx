8fab066f3febc7b81f863d6d5ad2a487e22ef108
Daegyeom Ha <combbm@gmail.com>
2023-11-12 21:18:30 +0900

    feat: implement sample game board

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,78 @@
+import { CityCard } from './components/CityCard';
 import './styles/global.css';
 
-import React, { useState } from 'react';
+const DUMMY_CITY_LIST = [
+  {
+    nameKo: '서울',
+    nameEn: 'Seoul',
+    price: '1000000'
+  },
+  {
+    nameKo: '도쿄',
+    nameEn: 'Tokyo',
+    price: '1000000'
+  },
+  {
+    nameKo: '방콕',
+    nameEn: 'Bangkok',
+    price: '1000000'
+  },
+  {
+    nameKo: '베이징',
+    nameEn: 'Beijing',
+    price: '1000000'
+  },
+  {
+    nameKo: '베를린',
+    nameEn: 'Berlin',
+    price: '1000000'
+  },
+  {
+    nameKo: '로마',
+    nameEn: 'Roma',
+    price: '1000000'
+  }
+];
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <button type="button" onClick={() => setCount((prev) => prev + 1)}>
-        Counter
-      </button>
-      <p className="text-red-500 text-bold">{count}</p>
+    <div className="relative h-screen">
+      <div className="absolute w-full h-full flex flex-col justify-between items-center">
+        <div className="flex space-x-1 -mt-6">
+          {DUMMY_CITY_LIST.map((city) => (
+            <CityCard key={city.nameEn} nameKo={city.nameKo} nameEn={city.nameEn} price={city.price} />
+          ))}
+        </div>
+        <div className="flex space-x-1 -mb-6">
+          {DUMMY_CITY_LIST.map((city) => (
+            <CityCard key={city.nameEn} nameKo={city.nameKo} nameEn={city.nameEn} price={city.price} />
+          ))}
+        </div>
+      </div>
+      <div className="absolute w-full h-full flex flex-row justify-around items-center">
+        <div className="flex flex-col -ml-48">
+          {DUMMY_CITY_LIST.map((city) => (
+            <CityCard
+              className="-rotate-90 -mb-11 last:mb-0"
+              key={city.nameEn}
+              nameKo={city.nameKo}
+              nameEn={city.nameEn}
+              price={city.price}
+            />
+          ))}
+        </div>
+        <div className="flex flex-col -mr-48">
+          {DUMMY_CITY_LIST.map((city) => (
+            <CityCard
+              className="rotate-90 -mb-11 last:mb-0"
+              key={city.nameEn}
+              nameKo={city.nameKo}
+              nameEn={city.nameEn}
+              price={city.price}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
