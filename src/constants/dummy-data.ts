6ff8cce36c2c0ddb507ffe4b8d77f686fc4f279b
Daegyeom Ha <combbm@gmail.com>
2023-11-15 23:45:42 +0900

    feat: create dummy-data file

---
+++ src/constants/dummy-data.ts
@@ -0,0 +1,32 @@
+export const DUMMY_CITY_LIST = [
+  {
+    nameKo: '서울',
+    nameEn: 'Seoul',
+    price: 1000000
+  },
+  {
+    nameKo: '도쿄',
+    nameEn: 'Tokyo',
+    price: 1000000
+  },
+  {
+    nameKo: '방콕',
+    nameEn: 'Bangkok',
+    price: 1000000
+  },
+  {
+    nameKo: '베이징',
+    nameEn: 'Beijing',
+    price: 1000000
+  },
+  {
+    nameKo: '베를린',
+    nameEn: 'Berlin',
+    price: 1000000
+  },
+  {
+    nameKo: '로마',
+    nameEn: 'Roma',
+    price: 1000000
+  }
+];
