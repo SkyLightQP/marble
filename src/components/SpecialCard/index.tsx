f59768838dce8197fd55312d99f125e24bb4ca48
Daegyeom Ha <combbm@gmail.com>
2023-11-18 16:54:22 +0900

    fix: update size of SpecialCard component

--- src/components/SpecialCard/index.tsx
+++ src/components/SpecialCard/index.tsx
@@ -2,6 +2,6 @@ import React, { PropsWithChildren } from 'react';
 
 export const SpecialCard: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="w-36 h-36 border-2 border-black p-2 flex flex-col justify-center items-center">{children}</div>
+    <div className="w-32 h-32 border-2 border-black p-2 flex flex-col justify-center items-center">{children}</div>
   );
 };
