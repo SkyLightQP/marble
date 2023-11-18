5d9f3db2f9dd95867849daa287eafc8ad056b6f2
Daegyeom Ha <combbm@gmail.com>
2023-11-18 17:31:44 +0900

    feat: add background color

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import { GameBoard } from '../components/GameBoard';
 
 export const MainPage: React.FC = () => {
-  return <GameBoard />;
+  return (
+    <div className="bg-gray-50">
+      <GameBoard />
+    </div>
+  );
 };
