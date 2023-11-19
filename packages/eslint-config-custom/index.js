367eb70fc97b2a22e67c3fd28e2f04b1245a8dd7
Daegyeom Ha <combbm@gmail.com>
2023-11-20 01:38:00 +0900

    refactor: share eslint config and tsconfig

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,30 @@
+module.exports = {
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  extends: ['airbnb', 'plugin:react/jsx-runtime', 'plugin:@typescript-eslint/recommended', 'prettier', 'turbo'],
+  parser: '@typescript-eslint/parser',
+  globals: {
+    JSX: true
+  },
+  rules: {
+    'arrow-body-style': 'off',
+    'jsx-quotes': 'off',
+    'comma-dangle': 'off',
+    'object-curly-newline': 'off',
+    'linebreak-style': 'off',
+
+    'react/jsx-filename-extension': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/jsx-tag-spacing': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/function-component-definition': 'off',
+    'react/jsx-no-useless-fragment': 'off',
+    'react/require-default-props': 'off',
+
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off'
+  }
+};
