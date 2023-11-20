d75f83daf6d46fc7c73144aca4d70e15ce06a715
Daegyeom Ha <combbm@gmail.com>
2023-11-20 21:06:11 +0900

    fix: add COLOR_MAP for tailwindcss

--- apps/frontend/src/components/PlayerDot/index.tsx
+++ apps/frontend/src/components/PlayerDot/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import clsx from 'clsx';
 import { DotColor } from '../../types/DotColor';
 
 interface PlayerDotProps {
   readonly color: DotColor;
 }
 
+const COLOR_MAP = {
+  red: 'bg-red-500',
+  blue: 'bg-blue-500',
+  green: 'bg-green-500',
+  yellow: 'bg-yellow-500'
+};
+
 export const PlayerDot: React.FC<PlayerDotProps> = ({ color }) => {
-  return <div className={`w-3.5 h-3.5 rounded-full bg-${color}-500`} />;
+  const colorStyle = COLOR_MAP[color];
+  return <div className={clsx('w-3.5 h-3.5 rounded-full', colorStyle)} />;
 };
