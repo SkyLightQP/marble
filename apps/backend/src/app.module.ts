625276af858f3c245dd5fe887945c00f51a4d56a
Daegyeom Ha <combbm@gmail.com>
2023-11-23 21:08:05 +0900

    feat(backend): add helmet and add cors config

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: []
 })
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,4 +1,6 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { winstonLoggerConfig } from 'infrastructure/utils/logger.util';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './infrastructure/interceptors/response.interceptor';
@@ -7,7 +9,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: winstonLoggerConfig
   });
+  const config = app.get<ConfigService>(ConfigService);
+  const corsOrigins = JSON.stringify(config.get<string>('CORS_ORIGINS', '[]'));
 
+  app.use(helmet());
+  app.enableCors({
+    credentials: true,
+    origin: corsOrigins,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD']
+  });
   app.useGlobalInterceptors(new ResponseInterceptor());
 
   await app.listen(8080);
