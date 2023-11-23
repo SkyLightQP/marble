6253e776ecd07e409b1d101aa9fd21b8d5b531ee
Daegyeom Ha <combbm@gmail.com>
2023-11-23 20:33:27 +0900

    feat(backend): add ResponseInterceptor

---
+++ apps/backend/src/infrastructure/interceptors/response.interceptor.ts
@@ -0,0 +1,11 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Response } from '../common/types/response.type';
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
+    return next.handle().pipe(map((result) => ({ result })));
+  }
+}
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ResponseInterceptor } from './infrastructure/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(8080);
 }
 bootstrap();
