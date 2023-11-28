614682b2e968a3e9eb2e190fa24ffd89fa4f3add
Daegyeom Ha <combbm@gmail.com>
2023-11-28 21:17:01 +0900

    fix(backend): remove unused controller

--- apps/backend/src/app/user/user.module.ts
+++ apps/backend/src/app/user/user.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'infrastructure/database/database.module';
-import { UserController } from './controllers/user.controller';
 import { CreateUserHandler } from './handlers/create-user.handle';
 import { GetUserByUidHandler } from './handlers/get-user-by-uid.handler';
 import { GetUsersHandler } from './handlers/get-users.handler';
@@ -11,6 +10,6 @@ const queries = [GetUserByUidHandler, GetUsersHandler];
 @Module({
   imports: [DatabaseModule],
   providers: [...commands, ...queries],
-  controllers: [UserController]
+  controllers: []
 })
 export class UserModule {}
