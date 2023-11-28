f64aaef707b6b29ff89fa8c16838ec0e7405eeec
Daegyeom Ha <combbm@gmail.com>
2023-11-28 21:22:14 +0900

    fix(backend): add readonly keyword to argument of query

--- apps/backend/src/app/user/queries/get-users.query.ts
+++ apps/backend/src/app/user/queries/get-users.query.ts
@@ -1,5 +1,5 @@
 import { IQuery } from '@nestjs/cqrs';
 
 export class GetUsersQuery implements IQuery {
-  constructor(readonly args?: { limit: number; offset: number }) {}
+  constructor(readonly args?: { readonly limit: number; readonly offset: number }) {}
 }
