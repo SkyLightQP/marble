58becdd93f199838573e4b7bbf0a2e06f2646c43
Daegyeom Ha <combbm@gmail.com>
2023-11-29 21:06:59 +0900

    feat(backend): implement UserSignupListener to create user

--- apps/backend/src/app/user/user.module.ts
+++ apps/backend/src/app/user/user.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'infrastructure/database/database.module';
-import { CreateUserHandler } from './handlers/create-user.handle';
+import { CreateUserHandler } from './handlers/create-user.handler';
 import { GetUserByUidHandler } from './handlers/get-user-by-uid.handler';
 import { GetUsersHandler } from './handlers/get-users.handler';
+import { SignupUserListener } from './listeners/signup-user.listener';
 
 const commands = [CreateUserHandler];
 const queries = [GetUserByUidHandler, GetUsersHandler];
+const listeners = [SignupUserListener];
 
 @Module({
   imports: [DatabaseModule],
-  providers: [...commands, ...queries],
+  providers: [...commands, ...queries, ...listeners],
   controllers: []
 })
 export class UserModule {}
