d13071e34adcb150ae18281b8ccfa7963c3e3dbe
Daegyeom Ha <combbm@gmail.com>
2023-12-02 20:47:26 +0900

    refactor(backend): move signup logic to Auth module

--- apps/backend/src/app/auth/handlers/signup-user.handler.ts
+++ apps/backend/src/app/auth/handlers/signup-user.handler.ts
@@ -1,20 +1,48 @@
-import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
+import { ConflictException, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { hashSync } from 'bcrypt';
 import { DatabaseService } from 'infrastructure/database/database.service';
+import { ErrorCode } from 'infrastructure/error/error-code';
+import { prismaExclude } from 'infrastructure/utils/database.util';
 import { SignupUserCommand } from '../commands/signup-user.command';
 import { AuthTokenService } from '../services/auth-token.service';
 
 @CommandHandler(SignupUserCommand)
 export class SignupUserHandler implements ICommandHandler<SignupUserCommand> {
   constructor(
-    private readonly eventBus: EventBus,
+    private readonly config: ConfigService,
     private readonly authTokenService: AuthTokenService,
     private readonly prisma: DatabaseService
   ) {}
 
   async execute({ args: { id, password, nickname } }: SignupUserCommand) {
-    const user = await this.prisma.user.findUnique({ where: { id } });
-    const accessToken = this.authTokenService.generateAccessToken(user.userId, { id });
-    const refreshToken = this.authTokenService.generateRefreshToken(user.userId, { id });
+    const salt = Number(this.config.get('BCRYPT_SALT'));
+
+    const prevUser = await this.prisma.user.findUnique({
+      where: {
+        id,
+        nickname
+      }
+    });
+
+    if (prevUser !== null) {
+      throw new ConflictException(ErrorCode.USER_ALREADY_EXISTS);
+    }
+
+    const result = await this.prisma.user.create({
+      data: {
+        id,
+        nickname,
+        password: hashSync(password, salt)
+      },
+      select: prismaExclude('User', ['password'])
+    });
+
+    Logger.log({ message: '새로운 유저가 가입했습니다.', uid: result.userId, id: result.id });
+
+    const accessToken = this.authTokenService.generateAccessToken(result.userId, { id });
+    const refreshToken = this.authTokenService.generateRefreshToken(result.userId, { id });
 
     return {
       accessToken,
--- apps/backend/src/app/user/user.module.ts
+++ apps/backend/src/app/user/user.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'infrastructure/database/database.module';
-import { CreateUserHandler } from './handlers/create-user.handler';
 import { GetUserByUidHandler } from './handlers/get-user-by-uid.handler';
 import { GetUsersHandler } from './handlers/get-users.handler';
 
-const commands = [CreateUserHandler];
+const commands = [];
 const queries = [GetUserByUidHandler, GetUsersHandler];
 
 @Module({
