a8a3d100d692bdd15b4585fc1c2ff8dac9aedf4a
Daegyeom Ha <combbm@gmail.com>
2023-12-03 17:05:22 +0900

    feat(backend): add refresh token to cookie when logined

--- apps/backend/src/app/auth/controllers/auth.controller.ts
+++ apps/backend/src/app/auth/controllers/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
+import type { Response } from 'express';
 import { SigninUserCommand } from '../commands/signin-user.command';
 import { SignupUserCommand } from '../commands/signup-user.command';
+import { SignupUserReturn } from '../handlers/signup-user.handler';
 import { SigninUserDto } from './dto/signin-user.dto';
 import { SignupUserDto } from './dto/signup-user.dto';
 
@@ -10,14 +12,24 @@ export class AuthController {
   constructor(private readonly commandBus: CommandBus) {}
 
   @Post('/signup')
-  async signup(@Body() body: SignupUserDto) {
+  async signup(@Body() body: SignupUserDto, @Res({ passthrough: true }) response: Response) {
     const command = new SignupUserCommand(body);
-    return this.commandBus.execute(command);
+    const result = await this.commandBus.execute<SignupUserCommand, SignupUserReturn>(command);
+    response.cookie('refreshToken', result.refreshToken, {
+      httpOnly: true,
+      secure: true
+    });
+    return result;
   }
 
   @Post('/signin')
-  async signin(@Body() body: SigninUserDto) {
+  async signin(@Body() body: SigninUserDto, @Res({ passthrough: true }) response: Response) {
     const command = new SigninUserCommand(body);
-    return this.commandBus.execute(command);
+    const result = await this.commandBus.execute(command);
+    response.cookie('refreshToken', result.refreshToken, {
+      httpOnly: true,
+      secure: true
+    });
+    return result;
   }
 }
