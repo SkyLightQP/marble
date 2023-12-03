7f6a98f25d66374de94866f88068da0059adb916
Daegyeom Ha <combbm@gmail.com>
2023-12-03 20:55:12 +0900

    feat(backend): implement JwtGuard to authenticate token

---
+++ apps/backend/src/infrastructure/guards/jwt.guard.ts
@@ -0,0 +1,25 @@
+import { AuthTokenService } from '@app/auth/services/auth-token.service';
+import { ErrorCode } from '@infrastructure/error/error-code';
+import { CanActivate, ExecutionContext, Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { JsonWebTokenError } from '@nestjs/jwt';
+
+@Injectable()
+export class JwtGuard implements CanActivate {
+  constructor(private readonly authTokenService: AuthTokenService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers.authorization;
+    const token = authHeader ? authHeader.split(' ')[1] : '';
+
+    try {
+      const decode = this.authTokenService.verifyAccessToken(token);
+      request.user = decode;
+    } catch (e) {
+      if (e instanceof JsonWebTokenError) throw new UnauthorizedException(ErrorCode.PERMISSION_DENIED);
+      Logger.error(e);
+    }
+
+    return true;
+  }
+}
