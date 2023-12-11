e50e23be72d26dafd4cd02c23bce96a70cd141b9
Daegyeom Ha <combbm@gmail.com>
2023-12-11 17:03:29 +0900

    fix(backend): remove test api

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,12 +1,6 @@
-import { DatabaseService } from '@infrastructure/database/database.service';
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly prisma: DatabaseService) {}
-
-  @Get('/')
-  async getTestUsers() {
-    return this.prisma.user.findMany();
-  }
+  constructor() {}
 }
