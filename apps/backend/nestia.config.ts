ff45dcfdcd500f661bddcfb85fce0e367f120143
Daegyeom Ha <combbm@gmail.com>
2023-12-12 21:04:04 +0900

    fix(api, backend): use distribute option instead of vite

--- apps/backend/nestia.config.ts
+++ apps/backend/nestia.config.ts
@@ -1,12 +1,8 @@
-import { AppModule } from '@app.module';
 import { INestiaConfig } from '@nestia/sdk';
-import { NestFactory } from '@nestjs/core';
 
-const NESTIA_CONFIG: INestiaConfig = {
-  input: async () => {
-    const app = await NestFactory.create(AppModule);
-    return app;
-  },
-  output: './src/api'
+export const NESTIA_CONFIG: INestiaConfig = {
+  input: ['src/app/**/controllers/*.controller.ts'],
+  output: 'src/api',
+  distribute: '../../packages/api'
 };
 export default NESTIA_CONFIG;
