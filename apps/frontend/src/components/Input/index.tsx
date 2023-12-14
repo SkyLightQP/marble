198be74357b221b1065f933d174742f4fb5948a5
Daegyeom Ha <combbm@gmail.com>
2023-12-14 21:07:24 +0900

    feat(frontend): add forwardRef() to Input component

--- apps/frontend/src/components/Input/index.tsx
+++ apps/frontend/src/components/Input/index.tsx
@@ -1,6 +1,12 @@
 import clsx from 'clsx';
-import React, { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, forwardRef } from 'react';
 
-export const Input: React.FC<InputHTMLAttributes<never>> = ({ className, ...props }) => {
-  return <input className={clsx('w-56 h-10 bg-white border-2 border-gray-300 rounded-md p-3', className)} {...props} />;
-};
+export const Input = forwardRef<HTMLInputElement, InputHTMLAttributes<never>>(({ className, ...props }, ref) => {
+  return (
+    <input
+      ref={ref}
+      className={clsx('w-56 h-10 bg-white border-2 border-gray-300 rounded-md p-3', className)}
+      {...props}
+    />
+  );
+});
