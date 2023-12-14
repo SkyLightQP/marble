d0bdc772a8a41af7e643a0a60c60e17db32fb17b
Daegyeom Ha <combbm@gmail.com>
2023-12-14 21:22:20 +0900

    feat(frontend): add Toaster component to use toast

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -2,11 +2,13 @@ import './styles/global.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
 import { Router } from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Router />
+    <Toaster />
   </React.StrictMode>
 );
