17dd2dd6c2b7465ed3b7f4fe4a5adb29d65d1c14
Daegyeom Ha <combbm@gmail.com>
2023-12-14 21:22:36 +0900

    feat(frontend): implement login logic

--- apps/frontend/src/pages/LoginPage.tsx
+++ apps/frontend/src/pages/LoginPage.tsx
@@ -1,10 +1,33 @@
+import api from '@marble/api';
 import React from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 import { Button } from '../components/Button';
 import { Input } from '../components/Input';
 import { Label } from '../components/Label';
+import { API_HOST } from '../constants/api';
 import { RootLayout } from '../layouts/RootLayout';
+import { getCustomError } from '../utils/error-util';
+
+interface LoginForm {
+  readonly id: string;
+  readonly password: string;
+}
 
 export const LoginPage: React.FC = () => {
+  const { register, handleSubmit } = useForm<LoginForm>();
+
+  const onLoginButtonClick: SubmitHandler<LoginForm> = async (data) => {
+    try {
+      const result = await api.functional.auth.signin({ host: API_HOST }, { id: data.id, password: data.password });
+      localStorage.setItem('accessToken', result.accessToken);
+      toast('로그인 성공!');
+    } catch (e) {
+      const error = getCustomError(e);
+      toast.error(error.message);
+    }
+  };
+
   return (
     <RootLayout className="w-screen h-screen">
       <div className="h-full flex flex-col justify-center items-center text-center">
@@ -15,16 +38,24 @@ export const LoginPage: React.FC = () => {
 
         <div className="mb-3">
           <Label htmlFor="loginPage-id">아이디</Label>
-          <Input id="loginPage-id" type="text" placeholder="아이디" />
+          <Input id="loginPage-id" type="text" placeholder="아이디" {...register('id', { required: true })} />
         </div>
 
         <div className="mb-5">
           <Label htmlFor="loginPage-password">비밀번호</Label>
-          <Input id="loginPage-password" type="password" placeholder="비밀번호" autoComplete="off" />
+          <Input
+            id="loginPage-password"
+            type="password"
+            placeholder="비밀번호"
+            autoComplete="off"
+            {...register('password', { required: true })}
+          />
         </div>
 
         <div>
-          <Button className="w-56 h-10">로그인</Button>
+          <Button className="w-56 h-10" onClick={handleSubmit(onLoginButtonClick)}>
+            로그인
+          </Button>
         </div>
       </div>
     </RootLayout>
