b685f28fdfae308b428176641531c5d999596a8b
Daegyeom Ha <combbm@gmail.com>
2023-12-16 15:51:08 +0900

    feat(frontend): add message props to InputError component
    * Add `message` prop for validate option.
    * If the type is `validate`, compare `message` prop to show error message.

--- apps/frontend/src/components/InputError/index.tsx
+++ apps/frontend/src/components/InputError/index.tsx
@@ -4,8 +4,16 @@ import { FieldError, RegisterOptions } from 'react-hook-form';
 interface InputErrorProps {
   readonly formError: FieldError | undefined;
   readonly type: keyof RegisterOptions;
+  readonly message?: string;
 }
 
-export const InputError: React.FC<PropsWithChildren<InputErrorProps>> = ({ formError, type, children }) => {
+export const InputError: React.FC<PropsWithChildren<InputErrorProps>> = ({ formError, type, message, children }) => {
+  if (type === 'validate' && formError?.message === message) {
+    return <p className="text-xs text-red-400 mt-1 ml-[6px] text-left">{children}</p>;
+  }
+  if (type === 'validate' && formError?.message !== message) {
+    return <></>;
+  }
+
   return <>{formError?.type === type && <p className="text-xs text-red-400 mt-1 ml-[6px] text-left">{children}</p>}</>;
 };
