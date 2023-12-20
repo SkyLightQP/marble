fee8370f225986fd680382bd289bb508cb9f161a
Daegyeom Ha <combbm@gmail.com>
2023-12-20 21:09:33 +0900

    feat(backend): add host, port, username env for redis

--- apps/backend/src/infrastructure/redis/redis.service.ts
+++ apps/backend/src/infrastructure/redis/redis.service.ts
@@ -1,11 +1,18 @@
+import { ConfigService } from '@nestjs/config';
 import { createClient } from 'redis';
 
 export const REDIS_PROVIDER = [
   {
     provide: 'REDIS_CLIENT',
-    useFactory: async () => {
+    useFactory: async (config: ConfigService) => {
+      const host = config.get<string>('REDIS_HOST', 'localhost');
+      const port = config.get<number>('REDIS_PORT', 6379);
+      const username = config.get('REDIS_USERNAME', undefined);
+      const password = config.get('REDIS_PASSWORD', undefined);
       const client = createClient({
-        url: 'redis://localhost:6379'
+        url: `redis://${host}:${port}`,
+        username,
+        password
       });
       await client.connect();
       return client;
