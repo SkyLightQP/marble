04b759b04913f5e7db16095f271258195070da4d
Daegyeom Ha <combbm@gmail.com>
2023-12-22 21:18:26 +0900

    fix(backend): remove redis username env

--- apps/backend/src/infrastructure/redis/redis.service.ts
+++ apps/backend/src/infrastructure/redis/redis.service.ts
@@ -7,11 +7,9 @@ export const REDIS_PROVIDER = [
     useFactory: async (config: ConfigService) => {
       const host = config.get<string>('REDIS_HOST', 'localhost');
       const port = config.get<number>('REDIS_PORT', 6379);
-      const username = config.get('REDIS_USERNAME', undefined);
       const password = config.get('REDIS_PASSWORD', undefined);
       const client = createClient({
         url: `redis://${host}:${port}`,
-        username,
         password
       });
       await client.connect();
