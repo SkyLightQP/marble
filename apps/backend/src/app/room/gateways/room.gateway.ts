9e9c45847beddfc00fe5f3286ba9ac892c21736f
Daegyeom Ha <combbm@gmail.com>
2023-12-26 14:32:20 +0900

    feat(backend): implement join-room, quit-room and get-rooms gateways

--- apps/backend/src/app/room/gateways/room.gateway.ts
+++ apps/backend/src/app/room/gateways/room.gateway.ts
@@ -1,15 +1,33 @@
-import { Logger } from '@nestjs/common';
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import type { Server } from 'socket.io';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { MessageBody, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { JoinRoomCommand } from '../commands/join-room.command';
+import { QuitRoomCommand } from '../commands/quit-room.command';
+import { GetRoomsReturn } from '../handlers/get-rooms.handler';
+import { JoinRoomReturn } from '../handlers/join-room.handler';
+import { QuitRoomReturn } from '../handlers/quit-room.handler';
+import { GetRoomsQuery } from '../queries/get-rooms.query';
+import { JoinRoomDto } from './dto/join-room.dto';
+import { QuitRoomDto } from './dto/quit-room.dto';
 
 @WebSocketGateway({ cors: true })
 export class RoomGateway {
-  @WebSocketServer()
-  private server!: Server;
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus
+  ) {}
 
-  @SubscribeMessage('join')
-  handleMessage(@MessageBody() message: string): boolean {
-    Logger.debug(message);
-    return true;
+  @SubscribeMessage('join-room')
+  async handleJoinRoom(@MessageBody() message: JoinRoomDto): Promise<JoinRoomReturn> {
+    return this.commandBus.execute(new JoinRoomCommand(message));
+  }
+
+  @SubscribeMessage('quit-room')
+  async handleQuitRoom(@MessageBody() message: QuitRoomDto): Promise<QuitRoomReturn> {
+    return this.commandBus.execute(new QuitRoomCommand(message));
+  }
+
+  @SubscribeMessage('get-rooms')
+  async handleGetRooms(): Promise<GetRoomsReturn> {
+    return this.queryBus.execute(new GetRoomsQuery());
   }
 }
