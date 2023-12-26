a5d8df9bcb06c668018e65319b0f895fe747c610
Daegyeom Ha <combbm@gmail.com>
2023-12-26 15:49:04 +0900

    feat(backend): add error handler in syncRedis() method

--- apps/backend/src/infrastructure/common/abstract/syncable-to-redis.ts
+++ apps/backend/src/infrastructure/common/abstract/syncable-to-redis.ts
@@ -6,6 +6,10 @@ export abstract class SyncableToRedis {
   abstract toJSON(): unknown;
 
   protected async syncRedis(redis: RedisClientType, table: string, key: string): Promise<void> {
-    await redis.hSet(table, key, this.toString());
+    try {
+      await redis.hSet(table, key, this.toString());
+    } catch (error) {
+      throw new Error(`Failed to sync ${table} to redis: ${error}`);
+    }
   }
 }
