358b885e108baf6196ad158e02969b127db32adc
Daegyeom Ha <combbm@gmail.com>
2023-12-26 21:22:48 +0900

    feat(frontend): implement useSocket hooks

---
+++ apps/frontend/src/hooks/useSocket.tsx
@@ -0,0 +1,28 @@
+import React, { createContext, PropsWithChildren, useContext, useEffect, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
+import { API_HOST } from '../constants/api';
+
+const context = createContext<Socket | undefined>(undefined);
+const accessToken = typeof window !== 'undefined' ? localStorage.getItem('accessToken') : '';
+
+export const SocketProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  const [socket, setSocket] = useState<Socket | undefined>(undefined);
+
+  useEffect(() => {
+    const client = io(API_HOST, {
+      query: {
+        token: accessToken
+      }
+    });
+    client.connect();
+    setSocket(client);
+
+    return () => {
+      client.disconnect();
+    };
+  }, [accessToken]);
+
+  return <context.Provider value={socket}>{children}</context.Provider>;
+};
+
+export const useSocket = () => useContext(context);
--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -5,13 +5,16 @@ import ReactDOM from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
 import { Router } from './Router';
 import { UserProvider } from './hooks/useUser';
+import { SocketProvider } from './hooks/useSocket';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <UserProvider>
-      <Router />
-      <Toaster />
+      <SocketProvider>
+        <Router />
+      </SocketProvider>
     </UserProvider>
+    <Toaster />
   </React.StrictMode>
 );
