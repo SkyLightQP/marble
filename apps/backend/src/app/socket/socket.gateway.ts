751ac560789c8f7f32a1e32a94532f7b2d1d72d7
Daegyeom Ha <combbm@gmail.com>
2023-12-31 20:26:37 +0900

    feat(backend): implement SocketModule to manage room in socketio

---
+++ apps/backend/src/app/socket/socket.gateway.ts
@@ -0,0 +1,32 @@
+import { ConnectedSocket, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { BadRequestException, UseFilters, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { SocketJwtGuard } from '@infrastructure/guards/socket-jwt.guard';
+import type { Socket } from 'socket.io';
+import { ErrorCode } from '@infrastructure/error/error-code';
+import { WebsocketExceptionFilter } from '@infrastructure/filters/websocket-exception.filter';
+
+@WebSocketGateway({ cors: true })
+@UsePipes(
+  new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    transformOptions: {
+      enableImplicitConversion: true
+    },
+    exceptionFactory: () => new BadRequestException(ErrorCode.BAD_REQUEST)
+  })
+)
+@UseFilters(new WebsocketExceptionFilter())
+export class SocketGateway {
+  @UseGuards(SocketJwtGuard)
+  @SubscribeMessage('util:join-lobby')
+  handleJoinLobby(@ConnectedSocket() socket: Socket): void {
+    socket.join('lobby');
+  }
+
+  @UseGuards(SocketJwtGuard)
+  @SubscribeMessage('util:quit-lobby')
+  handleQuitLobby(@ConnectedSocket() socket: Socket): void {
+    socket.leave('lobby');
+  }
+}
---
+++ apps/backend/src/app/socket/socket.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { SocketGateway } from '@app/socket/socket.gateway';
+
+@Module({
+  providers: [SocketGateway],
+  controllers: []
+})
+export class SocketModule {}
