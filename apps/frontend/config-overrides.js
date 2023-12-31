789de9ef6d5d4b87ff8cd94cf100b52e6e83f61c
Daegyeom Ha <combbm@gmail.com>
2023-12-31 18:58:30 +0900

    chore(frontend): add absolute path setting

---
+++ apps/frontend/config-overrides.js
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { override, addWebpackAlias } = require('customize-cra');
+const path = require('path');
+
+module.exports = override(
+  addWebpackAlias({
+    '@': path.resolve(__dirname, 'src/')
+  })
+);
