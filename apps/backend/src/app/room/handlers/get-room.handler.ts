cbdff65b4a6420f32be042572a3da156db9ff4db
Daegyeom Ha <combbm@gmail.com>
2023-12-31 21:01:47 +0900

    fix(backend): check if room is exists

--- apps/backend/src/app/room/handlers/get-room.handler.ts
+++ apps/backend/src/app/room/handlers/get-room.handler.ts
@@ -2,6 +2,8 @@ import { Inject } from '@nestjs/common';
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { RedisClientType } from 'redis';
 import { GetRoomQuery } from '@app/room/queries/get-room.query';
+import { WsException } from '@nestjs/websockets';
+import { ErrorCode } from '@infrastructure/error/error-code';
 import { Room } from '../domain/room';
 
 export type GetRoomReturn = Room;
@@ -12,6 +14,11 @@ export class GetRoomHandler implements IQueryHandler<GetRoomQuery> {
 
   async execute({ args: { roomId } }: GetRoomQuery): Promise<GetRoomReturn> {
     const roomsInRedis = await this.redis.hGetAll('room');
+
+    if (roomsInRedis[roomId] === undefined) {
+      throw new WsException(ErrorCode.ROOM_NOT_FOUND);
+    }
+
     return Room.fromJSON(roomsInRedis[roomId]);
   }
 }
