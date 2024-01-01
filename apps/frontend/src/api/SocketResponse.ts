b1a0c8894eeba5a394fedbf908d9b045e4013938
Daegyeom Ha <combbm@gmail.com>
2024-01-01 16:42:04 +0900

    feat(frontend): add CreateRoomResponse type

--- apps/frontend/src/api/SocketResponse.ts
+++ apps/frontend/src/api/SocketResponse.ts
@@ -13,3 +13,12 @@ export interface WebSocketError {
   readonly code: string;
   readonly message: string;
 }
+
+export interface CreateRoomResponse {
+  readonly id: string;
+  readonly name: string;
+  readonly owner: string;
+  readonly players: string[];
+  readonly maxPlayer: number;
+  readonly isPlaying: boolean;
+}
