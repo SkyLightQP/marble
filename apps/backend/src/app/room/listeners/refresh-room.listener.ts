9c1e2b73d8836dc796643aadb0a1929241932c6e
Daegyeom Ha <combbm@gmail.com>
2024-01-11 21:18:14 +0900

    fix(backend): send get-rooms socket when DestroyedRoomEvent received

--- apps/backend/src/app/room/listeners/refresh-room.listener.ts
+++ apps/backend/src/app/room/listeners/refresh-room.listener.ts
@@ -1,10 +1,11 @@
 import { EventsHandler, IEventHandler, QueryBus } from '@nestjs/cqrs';
 import { CreatedRoomEvent } from '@/app/room/events/created-room.event';
+import { DestroyedRoomEvent } from '@/app/room/events/destroyed-room-event';
 import { GetRoomsQuery } from '@/app/room/queries/get-rooms.query';
 import { SocketGateway } from '@/app/socket/socket.gateway';
 
-@EventsHandler(CreatedRoomEvent)
-export class CreatedRoomListener implements IEventHandler<CreatedRoomEvent> {
+@EventsHandler(CreatedRoomEvent, DestroyedRoomEvent)
+export class RefreshRoomListener implements IEventHandler<CreatedRoomEvent> {
   constructor(
     private readonly socketGateway: SocketGateway,
     private readonly queryBus: QueryBus
--- apps/backend/src/app/room/room.module.ts
+++ apps/backend/src/app/room/room.module.ts
@@ -5,14 +5,14 @@ import { GetRoomHandler } from '@/app/room/handlers/get-room.handler';
 import { GetRoomsHandler } from '@/app/room/handlers/get-rooms.handler';
 import { JoinRoomHandler } from '@/app/room/handlers/join-room.handler';
 import { QuitRoomHandler } from '@/app/room/handlers/quit-room.handler';
-import { CreatedRoomListener } from '@/app/room/listeners/room.listener';
+import { RefreshRoomListener } from '@/app/room/listeners/refresh-room.listener';
 import { SocketModule } from '@/app/socket/socket.module';
 import { DatabaseModule } from '@/infrastructure/database/database.module';
 import { RedisModule } from '@/infrastructure/redis/redis.module';
 
 const commands = [JoinRoomHandler, QuitRoomHandler, CreateRoomHandler];
 const queries = [GetRoomsHandler, GetRoomHandler];
-const listeners = [CreatedRoomListener];
+const listeners = [RefreshRoomListener];
 const gateways = [RoomGateway];
 
 @Module({
