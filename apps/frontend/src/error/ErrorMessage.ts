cf928359ceeef507b890b62a427e3f0293e3627b
Daegyeom Ha <combbm@gmail.com>
2024-01-20 16:17:13 +0900

    feat(frontend): add error messages

--- apps/frontend/src/error/ErrorMessage.ts
+++ apps/frontend/src/error/ErrorMessage.ts
@@ -1,4 +1,6 @@
 const errorMessages: Record<string, string> = {
+  ROOM_IS_PLAYING: '게임이 시작된 방입니다.',
+  IS_NOT_OWNER: '방장이 아닙니다.',
   ROOM_IS_FULL: '방이 꽉 찼습니다.',
   PLAYER_ALREADY_EXISTS: '이미 존재하는 플레이어입니다.',
   PLAYER_NOT_FOUND: '플레이어를 찾을 수 없습니다.',
