32deb9a9893de329f76d50e7923c577336c9ca28
Daegyeom Ha <combbm@gmail.com>
2024-01-25 20:52:42 +0900

    feat(backend): add return type of GetGameQuery

--- apps/backend/src/app/game/handlers/get-game.handler.ts
+++ apps/backend/src/app/game/handlers/get-game.handler.ts
@@ -6,11 +6,13 @@ import { Game } from '@/app/game/domain/game';
 import { GetGameQuery } from '@/app/game/queries/get-game.query';
 import { ErrorCode } from '@/infrastructure/error/error-code';
 
+export type GetGameReturn = Game;
+
 @QueryHandler(GetGameQuery)
 export class GetGameHandler implements IQueryHandler<GetGameQuery> {
   constructor(@Inject('REDIS_CLIENT') private readonly redis: RedisClientType) {}
 
-  async execute({ args: { roomId } }: GetGameQuery) {
+  async execute({ args: { roomId } }: GetGameQuery): Promise<GetGameReturn> {
     const gameInRedis = await this.redis.hGet('game', roomId);
 
     if (gameInRedis === undefined) {
