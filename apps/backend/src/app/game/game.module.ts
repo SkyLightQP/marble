bb5ebcaa9709eda0e86ecc229ac0632ee212e9e1
Daegyeom Ha <combbm@gmail.com>
2024-01-26 20:42:14 +0900

    feat(backend): implement DropoutGameCommand

--- apps/backend/src/app/game/game.module.ts
+++ apps/backend/src/app/game/game.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GameGateway } from '@/app/game/gateways/game.gateway';
+import { DropoutGameHandler } from '@/app/game/handlers/dropout-game.handler';
 import { GetGameHandler } from '@/app/game/handlers/get-game.handler';
 import { StartGameHandler } from '@/app/game/handlers/start-game.handler';
 import { SocketModule } from '@/app/socket/socket.module';
 import { DatabaseModule } from '@/infrastructure/database/database.module';
 import { RedisModule } from '@/infrastructure/redis/redis.module';
 
-const commands = [StartGameHandler];
+const commands = [StartGameHandler, DropoutGameHandler];
 const queries = [GetGameHandler];
 const listeners = [];
 const gateways = [GameGateway];
---
+++ apps/backend/src/app/game/handlers/dropout-game.handler.ts
@@ -0,0 +1,36 @@
+import { Inject, Logger } from '@nestjs/common';
+import { CommandHandler, EventBus, ICommandHandler, QueryBus } from '@nestjs/cqrs';
+import { RedisClientType } from 'redis';
+import { DropoutGameCommand } from '@/app/game/commands/dropout-game.command';
+import { Room } from '@/app/room/domain/room';
+import { DestroyedRoomEvent } from '@/app/room/events/destroyed-room-event';
+import { GetRoomReturn } from '@/app/room/handlers/get-room.handler';
+import { GetRoomQuery } from '@/app/room/queries/get-room.query';
+
+export type DropoutGameReturn = Room | null;
+
+@CommandHandler(DropoutGameCommand)
+export class DropoutGameHandler implements ICommandHandler<DropoutGameCommand> {
+  constructor(
+    @Inject('REDIS_CLIENT') private readonly redis: RedisClientType,
+    private readonly queryBus: QueryBus,
+    private readonly eventBus: EventBus
+  ) {}
+
+  async execute({ args: { roomId, userId } }: DropoutGameCommand): Promise<DropoutGameReturn> {
+    const room = await this.queryBus.execute<GetRoomQuery, GetRoomReturn>(new GetRoomQuery({ roomId }));
+
+    room.removePlayer(userId);
+    await room.syncRedis(this.redis);
+    Logger.log({ message: '플레이어가 중퇴했습니다.', room, userId });
+
+    if (room.players.length <= 0) {
+      await this.redis.hDel('room', roomId);
+      this.eventBus.publish(new DestroyedRoomEvent({ room }));
+      Logger.log({ message: '플레이어가 아무도 없어 게임을 종료하고 방을 삭제합니다.', roomId });
+      return null;
+    }
+
+    return room;
+  }
+}
