a3c7596250ab4d4522277490b59884be841c9808
Daegyeom Ha <combbm@gmail.com>
2024-02-01 20:58:37 +0900

    feat(backend): create RolledDiceEvent

---
+++ apps/backend/src/app/game/events/rolled-dice.event.ts
@@ -0,0 +1,11 @@
+import { IEvent } from '@nestjs/cqrs';
+import { Game } from '@/app/game/domain/game';
+
+export class RolledDiceEvent implements IEvent {
+  constructor(
+    readonly args: {
+      readonly game: Game;
+      readonly executor: string;
+    }
+  ) {}
+}
