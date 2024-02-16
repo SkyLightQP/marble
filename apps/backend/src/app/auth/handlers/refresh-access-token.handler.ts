f14697a1e2c5e59f835940c495bb9e7b38f6a72b
Daegyeom Ha <combbm@gmail.com>
2024-02-16 21:27:11 +0900

    fix(backend): fix return type of `/auth/refresh` api

--- apps/backend/src/app/auth/handlers/refresh-access-token.handler.ts
+++ apps/backend/src/app/auth/handlers/refresh-access-token.handler.ts
@@ -2,18 +2,18 @@ import { ForbiddenException, Logger } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { JsonWebTokenError } from '@nestjs/jwt';
 import { RefreshAccessTokenCommand } from '@/app/auth/commands/refresh-access-token.command';
-import { AuthTokenService } from '@/app/auth/services/auth-token.service';
+import { AuthTokenService, RefreshAccessTokenReturn } from '@/app/auth/services/auth-token.service';
 import { ErrorCode } from '@/infrastructure/error/error-code';
 
 @CommandHandler(RefreshAccessTokenCommand)
 export class RefreshAccessTokenHandler implements ICommandHandler<RefreshAccessTokenCommand> {
   constructor(private readonly authTokenService: AuthTokenService) {}
 
-  async execute({ args: { refreshToken } }: RefreshAccessTokenCommand) {
+  async execute({ args: { refreshToken } }: RefreshAccessTokenCommand): Promise<RefreshAccessTokenReturn> {
     try {
       const { accessToken: newAccessToken, sub } = await this.authTokenService.refreshAccessToken(refreshToken);
       Logger.log({ message: 'AccessToken을 연장했습니다.', userId: sub });
-      return newAccessToken;
+      return { accessToken: newAccessToken, sub };
     } catch (e) {
       if (e instanceof JsonWebTokenError) throw new ForbiddenException(ErrorCode.PERMISSION_DENIED);
       throw e;
