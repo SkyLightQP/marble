76ce5e1d2431a3b9baedf5ed41cae0d78a01cbb9
Daegyeom Ha <combbm@gmail.com>
2024-02-16 21:22:52 +0900

    fix(frontend): add `credentials` option

--- apps/frontend/src/api/index.ts
+++ apps/frontend/src/api/index.ts
@@ -4,6 +4,9 @@ export const API_HOST = process.env.REACT_APP_API_HOST || 'http://localhost:8080
 
 export const apiConnection: IConnection = {
   host: API_HOST,
+  options: {
+    credentials: 'include'
+  },
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${localStorage.getItem('accessToken')}`
