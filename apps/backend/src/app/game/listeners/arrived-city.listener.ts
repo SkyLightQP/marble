f742c06d2cf3ef6869b52a87e614fb52f6ab18ce
Daegyeom Ha <combbm@gmail.com>
2024-02-17 14:38:09 +0900

    feat(backend): publish EndedTurnEvent and add log message

--- apps/backend/src/app/game/listeners/arrived-city.listener.ts
+++ apps/backend/src/app/game/listeners/arrived-city.listener.ts
@@ -1,10 +1,11 @@
 import { Inject, Logger } from '@nestjs/common';
-import { EventsHandler, IEventHandler, QueryBus } from '@nestjs/cqrs';
+import { EventBus, EventsHandler, IEventHandler, QueryBus } from '@nestjs/cqrs';
 import { RedisClientType } from 'redis';
 import { GetCityByPositionReturn } from '@/app/city/handlers/get-city-by-position.handler';
 import { GetCityByPositionQuery } from '@/app/city/queries/get-city-by-position.query';
 import { CITY_PENALTY_RATIO } from '@/app/game/constants/city-penalty.constant';
 import { SPECIAL_CARD_POSITIONS } from '@/app/game/constants/game-board.constant';
+import { EndedTurnEvent } from '@/app/game/events/ended-turn.event';
 import { RolledDiceEvent } from '@/app/game/events/rolled-dice.event';
 import { SocketGateway } from '@/app/socket/socket.gateway';
 
@@ -12,28 +13,34 @@ import { SocketGateway } from '@/app/socket/socket.gateway';
 export class ArrivedCityListener implements IEventHandler<RolledDiceEvent> {
   constructor(
     private readonly queryBus: QueryBus,
+    private readonly eventBus: EventBus,
     private readonly socketGateway: SocketGateway,
     @Inject('REDIS_CLIENT') private readonly redis: RedisClientType
   ) {}
 
   async handle({ args: { game, position, executePlayer } }: RolledDiceEvent) {
+    Logger.log({ message: '도시에 도착했습니다.', executor: executePlayer.userId, position });
+
     if (SPECIAL_CARD_POSITIONS.includes(position)) {
       game.increaseCurrentOrderPlayerIndex();
       await game.syncRedis(this.redis);
+      this.eventBus.publish(new EndedTurnEvent({ game }));
+      Logger.log({ message: '특별한 액션 없이 턴을 끝냅니다.', executor: executePlayer.userId });
       return;
     }
-    Logger.log({ message: '도시에 도착했습니다.', executor: executePlayer.userId, position });
 
     const city = await this.queryBus.execute<GetCityByPositionQuery, GetCityByPositionReturn>(
       new GetCityByPositionQuery({ position })
     );
-    const socketId = executePlayer.socketClientId;
     const cityOwnerId = game.cityWhoHave[city.id];
+    const socketId = executePlayer.socketClientId;
     const playerStatus = game.playerStatus[executePlayer.userId];
 
     if (cityOwnerId === executePlayer.userId) {
       game.increaseCurrentOrderPlayerIndex();
       await game.syncRedis(this.redis);
+      this.eventBus.publish(new EndedTurnEvent({ game }));
+      Logger.log({ message: '내 도시에 도착해 턴을 끝냅니다.', executor: executePlayer.userId });
       return;
     }
 
