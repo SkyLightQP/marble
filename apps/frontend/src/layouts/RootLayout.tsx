833bc6a711a4f04dfdc9da7231a829efabf2b39e
Daegyeom Ha <combbm@gmail.com>
2024-02-18 15:25:26 +0900

    fix(frontend): remove blank

--- apps/frontend/src/layouts/RootLayout.tsx
+++ apps/frontend/src/layouts/RootLayout.tsx
@@ -2,5 +2,5 @@ import clsx from 'clsx';
 import { PropsWithChildren } from 'react';
 
 export const RootLayout: React.FC<PropsWithChildren<{ className?: string }>> = ({ children, className }) => {
-  return <div className={clsx(' bg-gray-50', className)}>{children}</div>;
+  return <div className={clsx('bg-gray-50', className)}>{children}</div>;
 };
