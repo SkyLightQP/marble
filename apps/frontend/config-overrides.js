062ed0823d9d1f9ec2757773b6360a408d601bc7
Daegyeom Ha <combbm@gmail.com>
2024-02-18 14:40:52 +0900

    fix(frontend): remove create-react-app

--- apps/frontend/config-overrides.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { override, addWebpackAlias } = require('customize-cra');
-const path = require('path');
-
-module.exports = override(
-  addWebpackAlias({
-    '@': path.resolve(__dirname, 'src/')
-  })
-);
