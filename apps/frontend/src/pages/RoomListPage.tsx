a3baeedac8d0c29a4d4308c58ff1a5ab0375a642
Daegyeom Ha <combbm@gmail.com>
2024-02-18 15:25:00 +0900

    fix(frontend): remove unused Fragment

--- apps/frontend/src/pages/RoomListPage.tsx
+++ apps/frontend/src/pages/RoomListPage.tsx
@@ -73,44 +73,39 @@ export const RoomListPage: React.FC = () => {
   };
 
   return (
-    <>
-      <RootLayout className="h-screen w-screen p-20">
-        <div className="mb-5 flex items-center">
-          <h1 className="mr-4 text-4xl font-bold">
-            방 목록<span className="ml-1 text-sm font-normal text-gray-400">({rooms.length})</span>
-          </h1>
-          <Button
-            className="mr-2 flex h-8 w-8 items-center justify-center text-2xl"
-            onClick={() => {
-              openModal(CreateRoomModal, {
-                onCreateRoomClick,
-                form: { register, handleSubmit, reset, errors }
-              });
-            }}
-          >
-            <RiAddFill />
-          </Button>
-          <Button
-            className="flex h-8 w-8 items-center justify-center text-xl"
-            onClick={() => socket?.emit('get-rooms')}
-          >
-            <RiRefreshLine />
-          </Button>
-        </div>
+    <RootLayout className="h-screen w-screen p-20">
+      <div className="mb-5 flex items-center">
+        <h1 className="mr-4 text-4xl font-bold">
+          방 목록<span className="ml-1 text-sm font-normal text-gray-400">({rooms.length})</span>
+        </h1>
+        <Button
+          className="mr-2 flex h-8 w-8 items-center justify-center text-2xl"
+          onClick={() => {
+            openModal(CreateRoomModal, {
+              onCreateRoomClick,
+              form: { register, handleSubmit, reset, errors }
+            });
+          }}
+        >
+          <RiAddFill />
+        </Button>
+        <Button className="flex h-8 w-8 items-center justify-center text-xl" onClick={() => socket?.emit('get-rooms')}>
+          <RiRefreshLine />
+        </Button>
+      </div>
 
-        <div className="grid grid-cols-4 gap-4">
-          {rooms.map((room) => (
-            <RoomPreviewCard
-              key={room.id}
-              name={room.name}
-              currentPlayer={room.players.length}
-              maxPlayer={room.maxPlayer}
-              isPlaying={room.isPlaying}
-              onClick={() => navigate(`/room/${room.id}`)}
-            />
-          ))}
-        </div>
-      </RootLayout>
-    </>
+      <div className="grid grid-cols-4 gap-4">
+        {rooms.map((room) => (
+          <RoomPreviewCard
+            key={room.id}
+            name={room.name}
+            currentPlayer={room.players.length}
+            maxPlayer={room.maxPlayer}
+            isPlaying={room.isPlaying}
+            onClick={() => navigate(`/room/${room.id}`)}
+          />
+        ))}
+      </div>
+    </RootLayout>
   );
 };
