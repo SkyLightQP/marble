711eeda44e0030ea7649f77e14d51afa2831e6c0
Daegyeom Ha <combbm@gmail.com>
2024-02-25 17:11:47 +0900

    refactor(backend): remove room and game in EndGameWhenDropoutListener

--- apps/backend/src/app/game/handlers/dropout-game.handler.ts
+++ apps/backend/src/app/game/handlers/dropout-game.handler.ts
@@ -6,7 +6,6 @@ import { DropoutGameEvent } from '@/app/game/events/dropout-game.event';
 import { GetGameReturn } from '@/app/game/handlers/get-game.handler';
 import { GetGameQuery } from '@/app/game/queries/get-game.query';
 import { Room } from '@/app/room/domain/room';
-import { DestroyedRoomEvent } from '@/app/room/events/destroyed-room.event';
 import { GetRoomReturn } from '@/app/room/handlers/get-room.handler';
 import { GetRoomQuery } from '@/app/room/queries/get-room.query';
 
@@ -27,21 +26,13 @@ export class DropoutGameHandler implements ICommandHandler<DropoutGameCommand> {
     room.removePlayer(userId);
     game.removePlayer(userId);
 
-    if (room.players.length > 1) game.increaseCurrentOrderPlayerIndex();
+    if (room.players.length >= 1) game.increaseCurrentOrderPlayerIndex();
     await room.syncRedis(this.redis);
     await game.syncRedis(this.redis);
 
     this.eventBus.publish(new DropoutGameEvent({ userId, room, game }));
 
     Logger.log({ message: '플레이어가 중퇴했습니다.', room, userId });
-
-    if (room.players.length <= 0) {
-      await this.redis.hDel('room', roomId);
-      await this.redis.hDel('game', roomId);
-      this.eventBus.publish(new DestroyedRoomEvent({ room }));
-      Logger.log({ message: '플레이어가 아무도 없어 게임을 종료하고 방을 삭제합니다.', roomId });
-      return room;
-    }
 
     return room;
   }
--- apps/backend/src/app/game/listeners/end-game-when-dropout.listener.ts
+++ apps/backend/src/app/game/listeners/end-game-when-dropout.listener.ts
@@ -1,12 +1,26 @@
+import { Inject, Logger } from '@nestjs/common';
 import { EventBus, EventsHandler, IEventHandler } from '@nestjs/cqrs';
+import { RedisClientType } from 'redis';
 import { DropoutGameEvent } from '@/app/game/events/dropout-game.event';
 import { EndedGameEvent } from '@/app/game/events/ended-game.event';
+import { DestroyedRoomEvent } from '@/app/room/events/destroyed-room.event';
 
 @EventsHandler(DropoutGameEvent)
 export class EndGameWhenDropoutListener implements IEventHandler<DropoutGameEvent> {
-  constructor(private readonly eventBus: EventBus) {}
+  constructor(
+    private readonly eventBus: EventBus,
+    @Inject('REDIS_CLIENT') private readonly redis: RedisClientType
+  ) {}
 
-  handle({ args: { game, room } }: DropoutGameEvent) {
+  async handle({ args: { game, room } }: DropoutGameEvent) {
+    if (room.players.length <= 0) {
+      await this.redis.hDel('room', room.id);
+      await this.redis.hDel('game', room.id);
+      this.eventBus.publish(new DestroyedRoomEvent({ room }));
+      Logger.log({ message: '플레이어가 아무도 없어 게임을 종료하고 방을 삭제합니다.', roomId: room.id });
+      return;
+    }
+
     if (game.getPlayersNotDisable().length <= 1) {
       this.eventBus.publish(new EndedGameEvent({ game, room }));
     }
